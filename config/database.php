c79cc6434d4934f507eec9e1e2d167aa979f4064
Leonel Urbina <leonel.urbina.hn@gmail.com>
2018-08-28 18:19:04 -0600

    use mysql-db

--- config/database.php
+++ config/database.php
@@ -48,11 +48,16 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', $host),
+            /*'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', $database),
             'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $password),
+            'password' => env('DB_PASSWORD', $password),*/
+            'host' =>  $host,
+            'port' =>  '3306',
+            'database' =>  $database,
+            'username' =>  $username,
+            'password' =>  $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
